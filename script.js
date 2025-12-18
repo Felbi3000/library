e30a9c94eefed5b6b60bc74d60d812394ec4ec70
Felbi3000 <74322100+Felbi3000@users.noreply.github.com>
2025-12-18 17:06:01 +0100

    add JS code
    Added first pseudo code and code examples for changes. Created first structure

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log("Ich bin da");
+const myLibrary = [];
+const container = document.querySelector("#bookList");
+
+function Book(id, title, author) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.id = Crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.read = false;
+}
+
+
+
+function updateBookList() {
+  // delete list elements
+  for (const item in myLibrary) {
+    container.removeChild(item);
+    // div.setAttribute("id", "theDiv");
+    // div.textContent = "Hello World!";
+    
+    // 
+  };
+  // create list elements
+  for (const item in myLibrary) {
+    // create li item with ID = item.id
+    // const div = document.createElement("div");
+
+    // create divs with classes title, author, read and remove button
+
+    // parentNode.appendChild(childNode)
+    // div.setAttribute("id", "theDiv");
+    // div.textContent = "Hello World!";
+    
+    // 
+  };
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+
+  updateBookList();
+}
+
+function deleteBook () {
+  // div.getAttribute("id");
+  updateBookList();
+}
+
+
+// read array
+// put item in div
